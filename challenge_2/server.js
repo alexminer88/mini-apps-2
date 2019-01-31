7da296098b4dc38320d2741c7c419a88cf99a467
Alex Miner <alex.miner@gmail.com>
2019-01-31 11:57:54 -0800

    Create server and build webpack

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = 3000;
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`);
+});
 
 
+module.exports = app;
